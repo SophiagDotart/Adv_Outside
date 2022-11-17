49db756b45c949ef51c4af7b3c0026ea4ab982e3
SophiagDotart <112538755+SophiagDotart@users.noreply.github.com>
2022-11-17 16:25:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                index: "./outside.tmj",
+                index: "./index.html", "./outside.tmj",
                 ...getMapsScripts(),
             },
         },
